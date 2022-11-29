174c9fb05f34093cadf26189382e3fe0fa883e06
Magdaline <mamoakohene@gmail.com>
2022-11-29 21:32:33 +0000

    section 3 challenge

--- hello-web-project/app.rb
+++ hello-web-project/app.rb
@@ -14,5 +14,11 @@ class Application < Sinatra::Base
       return "#{name_1}, #{name_2}, #{name_3}"
     end
 
+    post '/sort-names' do
+      names = params[:names]
+      sorted_names = names.split(",")
+      return sorted_names.sort.join(", ")
+    end
+
   end
 end
